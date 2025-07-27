d4fcf646f9b610f80bc0ebbca3ee35c11a26e801
yukten <yuk7829@gmail.com>
2025-07-27 18:16:43 +0900

    Postモデルを形だけ追加

---
+++ app/models/post.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  content    :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  user_id    :bigint           not null
+#
+# Indexes
+#
+#  index_posts_on_user_id  (user_id)
+#
+class Post < ApplicationRecord
+  has_many_attached :images
+
+  belongs_to :user
+end
