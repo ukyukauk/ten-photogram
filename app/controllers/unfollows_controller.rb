4a4e2dc73e00f7fb6ed3dc2065748a94072d4419
yukten <yuk7829@gmail.com>
2025-09-05 08:44:39 +0900

    整理

--- app/controllers/unfollows_controller.rb
+++ app/controllers/unfollows_controller.rb
@@ -1,11 +1,6 @@
 class UnfollowsController < ApplicationController
   before_action :authenticate_user!
 
-  # def create
-  #   current_user.unfollow!(params[:account_id])
-  #   redirect_to account_path(params[:account_id])
-  # end
-
     def create
     account = User.find(params[:account_id])
     current_user.unfollow!(account)
