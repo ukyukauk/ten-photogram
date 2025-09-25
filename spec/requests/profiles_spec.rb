2d26b39cdfed3264b50fab2324f848d837d38f2c
yukten <yuk7829@gmail.com>
2025-09-26 06:02:55 +0900

    profiles_spec

--- spec/requests/profiles_spec.rb
+++ spec/requests/profiles_spec.rb
@@ -1,17 +1,16 @@
 require 'rails_helper'
 
-RSpec.describe 'Followings', type: :request do
+RSpec.describe 'Profiles', type: :request do
   let(:user) { create(:user) }
-  let(:account) { create(:user) }
 
-  describe 'GET /followings' do
+  describe 'GET /profile' do
     context 'ログインしている場合' do
       before do
         sign_in user
       end
 
       it '200ステータスが返ってくる' do
-        get account_followings_path(account)
+        get profile_path
 
         expect(response).to have_http_status(200)
       end
