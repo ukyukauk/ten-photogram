bf41cee47352a6d312142234be2b31f716bfba79
yukten <yuk7829@gmail.com>
2026-01-14 20:28:21 +0900

    axios修正

--- app/javascript/modules/axios.js
+++ app/javascript/modules/axios.js
@@ -3,6 +3,23 @@ import Rails from "@rails/ujs";
 
 Rails.start();
 
-axios.defaults.headers.common["X-CSRF-Token"] = Rails.csrfToken();
+function csrfToken() {
+  // まず meta を優先（Turboで更新されるのはこっち）
+  const token = document.querySelector('meta[name="csrf-token"]')?.content;
+  if (token) return token;
+
+  // 念のため UJS からも
+  return Rails.csrfToken?.();
+}
+
+// ★毎リクエスト直前に最新トークンを付ける（これが最重要）
+axios.interceptors.request.use((config) => {
+  const token = csrfToken();
+  if (token) {
+    config.headers = config.headers || {};
+    config.headers["X-CSRF-Token"] = token;
+  }
+  return config;
+});
 
 export default axios;
