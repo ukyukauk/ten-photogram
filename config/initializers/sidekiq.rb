3a833a4b990e8384930f650f81d0b8358ce3e5f8
yukten <yuk7829@gmail.com>
2026-01-15 20:58:53 +0900

    no message

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,20 @@
+# config/initializers/sidekiq.rb
+redis_url = ENV.fetch("REDIS_URL", "redis://localhost:6379")
+
+redis_config =
+  if redis_url.start_with?("rediss://")
+    {
+      url: redis_url,
+      ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+    }
+  else
+    { url: redis_url }
+  end
+
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379') }
+  config.redis = redis_config
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379')}
+  config.redis = redis_config
 end
